48190db128501399024eb53ce2b0f93249e273ef
TrickyLiam <liamcarlson2014@gmail.com>
2023-08-01 21:57:36 -0500

    Add css to book/movie list

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,18 @@
+import React from 'react';
+import classes from './BookList.module.css';
+
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books";
+   let book1 = "https://m.media-amazon.com/images/I/91qXDU9TXvL.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/71LeGp9zFRL._AC_UF1000,1000_QL80_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/5179Q0So5NL.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Nothing here!" className={classes.bookImg} />
+         <img src={book2} alt="Nothing here!" className={classes.bookImg}/>
+         <img src={book3} alt="Nothing here!" className={classes.bookImg}/>
       </div>      
    );
 }
